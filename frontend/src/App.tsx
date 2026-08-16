41d15dd54be147bfa7f3888a3aff3bec9fee38fa
SonuPatelNL <sonupatelnlsonupatelnl@gmail.com>
2026-08-16 16:56:17 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
  import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import MeetingRoom from './pages/MeetingRoom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Home page with Create Meeting button */}
+        <Route path="/" element={<Home />} />
+        
+        {/* Meeting room page with ID in URL */}
         <Route path="/meeting/:meetingId" element={<MeetingRoom />} />
-        <Route path="/" element={<h1 style={{color:'white', textAlign:'center', padding:50}}>Go to /meeting/test123</h1>} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
