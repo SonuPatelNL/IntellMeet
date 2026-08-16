74ae502dd3a69faa20dc4314cc09aa70defdd92a
SonuPatelNL <sonupatelnlsonupatelnl@gmail.com>
2026-08-16 16:49:40 +0530

    Change route parameter from 'id' to 'meetingId'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/meeting/:id" element={<MeetingRoom />} />
+        <Route path="/meeting/:meetingId" element={<MeetingRoom />} />
         <Route path="/" element={<h1 style={{color:'white', textAlign:'center', padding:50}}>Go to /meeting/test123</h1>} />
       </Routes>
     </BrowserRouter>
